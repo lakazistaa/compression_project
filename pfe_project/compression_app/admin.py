c76df661d37edb7784d5d34e9acc64decfc3b097
Deghrar_Said <lakazistaa@gmail.com>
2024-05-10 13:51:05 +0100

    registering models in admin.py

--- pfe_project/compression_app/admin.py
+++ pfe_project/compression_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import *
+
+# Register each model with the admin site
+admin.site.register(Utilisateur)
+admin.site.register(Modele)
+admin.site.register(JeuDeDonnees)
+admin.site.register(MethodeCompression)
+admin.site.register(Tache)
+admin.site.register(ModeleCompresse)
+admin.site.register(ResultatEvaluation)
+admin.site.register(MetriqueCompression)
